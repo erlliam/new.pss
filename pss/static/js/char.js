8be19c9f8d9cb73d2abfac22688fa5c1ad9d335b
tommejas <tommejas@gmail.com>
2020-03-11 14:31:13 -0400

    Add new files to repo

---
+++ pss/static/js/char.js
@@ -0,0 +1,171 @@
+let character;
+let loadoutList = {
+    "1": "NC Infiltrator",
+    "3": "NC Light Assault",
+    "4": "NC Medic",
+    "5": "NC Engineer",
+    "6": "NC Heavy Assault",
+    "7": "NC MAX",
+    "8": "TR Infiltrator",
+    "10": "TR Light Assault",
+    "11": "TR Medic",
+    "12": "TR Engineer",
+    "13": "TR Heavy Assault",
+    "14": "TR MAX",
+    "15": "VS Infiltrator",
+    "17": "VS Light Assault",
+    "18": "VS Medic",
+    "19": "VS Engineer",
+    "20": "VS Heavy Assault",
+    "21": "VS MAX"
+};
+
+let sessionButton = document.getElementById("session-button");
+
+sessionButton.addEventListener("click", () => {
+    startSession();
+});
+
+function initializeCharacter(name) {
+    name = encodeURIComponent(name.toLowerCase());
+    // let data = getJSON(url); is this ever possible
+    let url = `character/?name.first_lower=${name}&c:show=character_id,name.first,faction_id,times.creation_date,times.minutes_played,battle_rank.value,prestige_level&c:join=faction^inject_at:faction^show:code_tag,characters_stat_history^list:1^terms:stat_name=kills%27stat_name=deaths^show:stat_name%27all_time^inject_at:stats,characters_online_status^show:online_status^inject_at:online&c:tree=start:stats^field:stat_name`;
+    // Wish I could just retrieve the json without a function here
+    getJSON(url, function(data) {
+        if (data.returned) {
+            let rawCharacter = data.character_list[0];
+            character = {
+                exists: true,
+                name: rawCharacter.name.first,
+                character_id: rawCharacter.character_id,
+                faction: rawCharacter.faction.code_tag,
+                faction_id: rawCharacter.faction_id,
+                // 2014-06-13 09:36:33.0 -> 2014-06-13
+                join_date: (rawCharacter.times.creation_date).split(" ")[0],
+                time_played: (rawCharacter.times.minutes_played/
+                              60).toFixed(1) + "h",
+                level: rawCharacter.battle_rank.value,
+                prestige: rawCharacter.prestige_level,
+                // I should test this code to make sure rawCharacter is accurate
+                // Constantly fails cause doesnt return online properly
+                "status": parseInt(rawCharacter.online.online_status) ?
+                          "Online" :
+                          "Offline",
+                sessKills: 0,
+                sessDeaths: 0
+            };
+
+            try {
+                character.kills = rawCharacter.stats.kills.all_time;
+                character.deaths = rawCharacter.stats.deaths.all_time;
+                character.kd = (character.kills/
+                                character.deaths).toFixed(1);
+            } catch(error) {
+                if (error instanceof TypeError) {
+                    character.kills = "N/A";
+                    character.deaths = "N/A";
+                    character.kd = "N/A";
+                }
+            }
+
+            populateResultsDiv();
+        } else {
+            console.log("Not found, handle this..");
+        }
+    });
+
+}
+
+function populateResultsDiv() {
+    let results = document.getElementById("results");
+    results.style.display = "block";
+    let session = document.getElementById("session");
+    session.style.display = "block";
+    let children = results.children;
+
+    // character has more keys than results has children, is this ineffecient?
+    for (let stat in character) {
+        let child = children.namedItem(stat);
+        if (child) {
+            child.textContent += character[stat];
+        }
+    }
+}
+
+function startSession() {
+    let webSocket = new WebSocket("wss://push.planetside2.com/streaming?environment=ps2&service-id=s:supafarma");
+    webSocket.onopen = function() {
+        let deathsCommand = {
+            service: "event",
+            action: "subscribe",
+            characters: [character.character_id],
+            eventNames: ["Death"]
+        };
+
+        webSocket.send(JSON.stringify(deathsCommand));
+
+        webSocket.onmessage = function(message) {
+            message = JSON.parse(message.data);
+            if (message.hasOwnProperty("payload")) {
+                handleKillData(message.payload);
+            } else {
+                console.log("run");
+            }
+        };
+    };
+}
+
+function handleKillData(payload) {
+    let eventResult, enemyCharacterId, enemyLoadoutId;
+    if (payload.attacker_character_id == character.character_id) {
+        enemyCharacterId = payload.character_id;
+        enemyLoadoutId = payload.character_loadout_id;
+        eventResult = "kill";
+        character.sessKills += 1;
+    } else {
+        enemyCharacterId = payload.attacker_character_id;
+        enemyLoadoutId = payload.attacker_loadout_id;
+        eventResult = "death";
+        character.sessDeaths += 1;
+    }
+
+    let characterUrl = `character/?character_id=${enemyCharacterId}&c:show=character_id,name.first,battle_rank.value,prestige_level&c:join=characters_stat_history^list:1^terms:stat_name=kills'stat_name=deaths^show:stat_name'all_time^inject_at:stats&c:tree=start:stats^field:stat_name`;
+    let weapUrl = `item/?item_id=${payload.attacker_weapon_id}&c:show=name.en,image_path`;
+
+    let killData = {
+        // Kill or death
+        eventResult: eventResult,
+        loadout: loadoutList[enemyLoadoutId]
+    };
+
+    getJSON(characterUrl, function(data) {
+        if (data.returned) {
+            let rawChar = data.character_list[0];
+            killData.name = rawChar.name.first;
+            killData.br = rawChar.battle_rank.value;
+            killData.prestige = rawChar.prestige_level;
+            killData.kd = (rawChar.stats.kills.all_time/
+                           rawChar.stats.deaths.all_time).toFixed(1);
+        }
+        getJSON(weapUrl, function(data) {
+            if (data.returned) {
+                let rawWeapon = data.item_list[0];
+                killData.weapName = rawWeapon.name.en;
+                killData.weapImgUrl = baseUrl+rawWeapon.image_path;
+                displayKillData(killData);
+            }
+        });
+    });
+}
+
+function displayKillData(killData) {
+    let session = document.getElementById("session-events");
+    let div = document.createElement("div");
+    div.className = killData.eventResult;
+    div.textContent = Object.values(killData).join(", ");
+
+    session.insertBefore(div, session.childNodes[0]);
+
+    console.log(`Kills: ${character.sessKills}, Deaths:${character.sessDeaths}`);
+    console.log(killData);
+}
--- s
+++ s
@@ -22,7 +22,6 @@ set printoptions=paper:letter
 set ruler
 set runtimepath=~/.vim,~/.vim/pack/default/start/gruvbox,/var/lib/vim/addons,/usr/share/vim/vimfiles,/usr/share/vim/vim81,/usr/share/vim/vimfiles/after,/var/lib/vim/addons/after,~/.vim/after
 set shiftwidth=4
-set shortmess=aoO
 set softtabstop=4
 set suffixes=.bak,~,.swp,.o,.info,.aux,.log,.dvi,.bbl,.blg,.brf,.cb,.ind,.idx,.ilg,.inx,.out,.toc
 set tabstop=4
@@ -35,10 +34,6 @@ if expand('%') == '' && !&modified && line('$') <= 1 && getline(1) == ''
   let s:wipebuf = bufnr('%')
 endif
 set shortmess=aoO
-badd +1 URLS
-badd +1 character.json
-badd +1 c
-badd +3 .git/COMMIT_EDITMSG
 badd +5 pss/__init__.py
 badd +14 pss/views/index.py
 badd +7 pss/templates/index/index.html
@@ -55,8 +50,12 @@ badd +170 pss/static/js/char.js
 argglobal
 silent! argdel *
 $argadd pss/__init__.py
-edit character.json
+edit pss/static/js/main.js
 set splitbelow splitright
+wincmd _ | wincmd |
+vsplit
+1wincmd h
+wincmd w
 set nosplitbelow
 set nosplitright
 wincmd t
@@ -64,6 +63,8 @@ set winminheight=0
 set winheight=1
 set winminwidth=0
 set winwidth=1
+exe 'vert 1resize ' . ((&columns * 106 + 106) / 213)
+exe 'vert 2resize ' . ((&columns * 106 + 106) / 213)
 argglobal
 setlocal keymap=
 setlocal noarabic
@@ -99,8 +100,8 @@ setlocal nodiff
 setlocal equalprg=
 setlocal errorformat=
 setlocal expandtab
-if &filetype != 'json'
-setlocal filetype=json
+if &filetype != 'javascript'
+setlocal filetype=javascript
 endif
 setlocal fixendofline
 setlocal foldcolumn=0
@@ -164,8 +165,8 @@ setlocal statusline=
 setlocal suffixesadd=
 setlocal swapfile
 setlocal synmaxcol=3000
-if &syntax != 'json'
-setlocal syntax=json
+if &syntax != 'javascript'
+setlocal syntax=javascript
 endif
 setlocal tabstop=4
 setlocal tagcase=
@@ -185,18 +186,150 @@ set nowrap
 setlocal nowrap
 setlocal wrapmargin=0
 silent! normal! zE
-let s:l = 1 - ((0 * winheight(0) + 27) / 54)
+let s:l = 54 - ((40 * winheight(0) + 26) / 53)
 if s:l < 1 | let s:l = 1 | endif
 exe s:l
 normal! zt
-1
+54
 normal! 0
+wincmd w
+argglobal
+if bufexists('pss/static/js/char.js') | buffer pss/static/js/char.js | else | edit pss/static/js/char.js | endif
+setlocal keymap=
+setlocal noarabic
+setlocal autoindent
+setlocal backupcopy=
+setlocal balloonexpr=
+setlocal nobinary
+setlocal nobreakindent
+setlocal breakindentopt=
+setlocal bufhidden=
+setlocal buflisted
+setlocal buftype=
+setlocal nocindent
+setlocal cinkeys=0{,0},0),:,0#,!^F,o,O,e
+setlocal cinoptions=
+setlocal cinwords=if,else,while,do,for,switch
+set colorcolumn=80
+setlocal colorcolumn=80
+setlocal comments=s1:/*,mb:*,ex:*/,://,b:#,:%,:XCOMM,n:>,fb:-
+setlocal commentstring=/*%s*/
+setlocal complete=.,w,b,u,t,i
+setlocal concealcursor=
+setlocal conceallevel=0
+setlocal completefunc=
+setlocal nocopyindent
+setlocal cryptmethod=
+setlocal nocursorbind
+setlocal nocursorcolumn
+setlocal nocursorline
+setlocal define=
+setlocal dictionary=
+setlocal nodiff
+setlocal equalprg=
+setlocal errorformat=
+setlocal expandtab
+if &filetype != 'javascript'
+setlocal filetype=javascript
+endif
+setlocal fixendofline
+setlocal foldcolumn=0
+setlocal foldenable
+setlocal foldexpr=0
+setlocal foldignore=#
+setlocal foldlevel=0
+setlocal foldmarker={{{,}}}
+setlocal foldmethod=manual
+setlocal foldminlines=1
+setlocal foldnestmax=20
+setlocal foldtext=foldtext()
+setlocal formatexpr=
+setlocal formatoptions=tcq
+setlocal formatlistpat=^\\s*\\d\\+[\\]:.)}\\t\ ]\\s*
+setlocal formatprg=
+setlocal grepprg=
+setlocal iminsert=0
+setlocal imsearch=-1
+setlocal include=
+setlocal includeexpr=
+setlocal indentexpr=
+setlocal indentkeys=0{,0},:,0#,!^F,o,O,e
+setlocal noinfercase
+setlocal iskeyword=@,48-57,_,192-255
+setlocal keywordprg=
+setlocal nolinebreak
+setlocal nolisp
+setlocal lispwords=
+set list
+setlocal list
+setlocal makeencoding=
+setlocal makeprg=
+setlocal matchpairs=(:),{:},[:]
+setlocal nomodeline
+setlocal modifiable
+setlocal nrformats=bin,octal,hex
+set number
+setlocal number
+setlocal numberwidth=4
+setlocal omnifunc=
+setlocal path=
+setlocal nopreserveindent
+setlocal nopreviewwindow
+setlocal quoteescape=\\
+setlocal noreadonly
+setlocal norelativenumber
+setlocal norightleft
+setlocal rightleftcmd=search
+setlocal noscrollbind
+setlocal shiftwidth=4
+setlocal noshortname
+setlocal signcolumn=auto
+setlocal nosmartindent
+setlocal softtabstop=4
+setlocal nospell
+setlocal spellcapcheck=[.?!]\\_[\\])'\"\	\ ]\\+
+setlocal spellfile=
+setlocal spelllang=en
+setlocal statusline=
+setlocal suffixesadd=
+setlocal swapfile
+setlocal synmaxcol=3000
+if &syntax != 'javascript'
+setlocal syntax=javascript
+endif
+setlocal tabstop=4
+setlocal tagcase=
+setlocal tags=
+setlocal termwinkey=
+setlocal termwinscroll=10000
+setlocal termwinsize=
+setlocal textwidth=0
+setlocal thesaurus=
+setlocal noundofile
+setlocal undolevels=-123456
+setlocal varsofttabstop=
+setlocal vartabstop=
+setlocal nowinfixheight
+setlocal nowinfixwidth
+set nowrap
+setlocal nowrap
+setlocal wrapmargin=0
+silent! normal! zE
+let s:l = 170 - ((51 * winheight(0) + 26) / 53)
+if s:l < 1 | let s:l = 1 | endif
+exe s:l
+normal! zt
+170
+normal! 0
+wincmd w
+exe 'vert 1resize ' . ((&columns * 106 + 106) / 213)
+exe 'vert 2resize ' . ((&columns * 106 + 106) / 213)
 tabnext 1
 if exists('s:wipebuf') && len(win_findbuf(s:wipebuf)) == 0
   silent exe 'bwipe ' . s:wipebuf
 endif
 unlet! s:wipebuf
-set winheight=1 winwidth=20 shortmess=aoO
+set winheight=1 winwidth=20 shortmess=filnxtToO
 set winminheight=1 winminwidth=1
 let s:sx = expand("<sfile>:p:r")."x.vim"
 if file_readable(s:sx)
