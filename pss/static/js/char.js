f27062d8a8ee60c71d1fdffd9d06cd0b719cf96f
tommejas <tommejas@gmail.com>
2020-03-25 02:11:10 -0400

    Refactored char.js
    The results of the code review have yet to be implemented.
    I will have to start refactoring outfit.js before this though.

--- pss/static/js/char.js
+++ pss/static/js/char.js
@@ -1,5 +1,4 @@
-let character;
-let loadoutList = {
+const loadoutList = {
     "1": "NC Infiltrator",
     "3": "NC Light Assault",
     "4": "NC Medic",
@@ -19,210 +18,215 @@ let loadoutList = {
     "20": "VS Heavy Assault",
     "21": "VS MAX"
 };
-// Script in body executed by flask templates
-let sessionEvents = document.getElementById("session-events");
-let sessionButton = document.getElementById("session-button");
-let timeElement = document.getElementById("session-time");
-let kdElement = document.getElementById("session-kd");
-let kpmElement = document.getElementById("session-kpm");
-let session = false;
-let sessionKills;
-let sessionDeaths;
-let timeElapsed;
-let timeInterval;
-let timeContainer;
-let webSocket;
+let characterId;
 
-sessionButton.addEventListener("click", () => {
-    session = !session;
-    sessionButton.classList.toggle("session-on", session);
-    sessionButton.textContent = session ? "End session" : "Start session";
+async function getCharacter(name) {
+    let url = `character/?name.first_lower=${encodeURIComponent(name.toLowerCase())}&c:show=character_id,name.first,faction_id,times.creation_date,times.minutes_played,battle_rank.value,prestige_level&c:join=faction^inject_at:faction^show:code_tag,characters_stat_history^list:1^terms:stat_name=kills%27stat_name=deaths^show:stat_name%27all_time^inject_at:stats,characters_online_status^show:online_status^inject_at:online&c:tree=start:stats^field:stat_name`;
 
-    if (session) {
-        startSession();
-    } else {
-        endSession();
-    }
-});
+    let response = await fetch(apiUrl + url)
+    let json = await response.json();
 
-function initializeCharacter(name) {
-    name = encodeURIComponent(name.toLowerCase());
-    let url = `character/?name.first_lower=${name}&c:show=character_id,name.first,faction_id,times.creation_date,times.minutes_played,battle_rank.value,prestige_level&c:join=faction^inject_at:faction^show:code_tag,characters_stat_history^list:1^terms:stat_name=kills%27stat_name=deaths^show:stat_name%27all_time^inject_at:stats,characters_online_status^show:online_status^inject_at:online&c:tree=start:stats^field:stat_name`;
-    getJSON(url, (data) => {
-        if (data.returned) {
-            let rawChar = (data.character_list ?? [])[0] ?? {};
-            character = {
-                exists: true,
-                name: (rawChar.name ?? {}).first ?? "N/A",
-                character_id: rawChar.character_id ?? "N/A",
-                faction: (rawChar.faction ?? {}).code_tag ?? "N/A",
-                faction_id: rawChar.faction_id ?? "N/A",
-                // 2014-06-13 09:36:33.0 -> 2014-06-13
-                join_date: ((rawChar.times ?? {}).creation_date ?? "").split(" ")[0],
-                // Parse string
-                time_played: (parseInt(((rawChar.times ?? {}).minutes_played ?? 0))/60)
-                             .toFixed(1) + "h",
-                level: (rawChar.battle_rank ?? {}).value ?? "N/A",
-                prestige: rawChar.prestige_level ?? "N/A",
-                "status": parseInt((rawChar.online ?? {}).online_status ?? 0)
-                          ? "Online" :
-                          "Offline",
-                kills: ((rawChar.stats ?? {}).kills ?? {}).all_time ?? 0,
-                deaths: ((rawChar.stats ?? {}).deaths ?? {}).all_time ?? 0,
-                sessKills: 0,
-                sessDeaths: 0
+    if (json.returned) {
+        let rawChar = (json.character_list  ?? [])[0] ?? {};
 
-            };
-            character.kd = (character.kills/character.deaths).toFixed(1);
-            populateResultsDiv();
-        } else {
-            let request = new Request("/flash");
-            let body = {
-                message: "User was not found. Check the name."
-            };
-
-            fetch(request, {
-                method: "POST",
-                headers: {
-                    "Content-type": "application/json"
-                },
-                body: JSON.stringify(body)
-            })
-            // .then() Handle error??
+        let character = {
+            exists: true,
+            name: (rawChar.name ?? {}).first ?? "N/A",
+            character_id: rawChar.character_id ?? "N/A",
+            faction: (rawChar.faction ?? {}).code_tag ?? "N/A",
+            faction_id: rawChar.faction_id ?? "N/A",
+            join_date: ((rawChar.times ?? {}).creation_date ?? "").split(" ")[0],
+            time_played: (parseInt(((rawChar.times ?? {}).minutes_played ?? 0))/60)
+                         .toFixed(1) + "h",
+            level: (rawChar.battle_rank ?? {}).value ?? "N/A",
+            prestige: rawChar.prestige_level ?? "N/A",
+            "status": parseInt((rawChar.online ?? {}).online_status ?? 0)
+                      ? "Online" :
+                      "Offline",
+            kills: ((rawChar.stats ?? {}).kills ?? {}).all_time ?? 0,
+            deaths: ((rawChar.stats ?? {}).deaths ?? {}).all_time ?? 0,
         }
-    });
+        character.kd = (character.kills/character.deaths).toFixed(1);
 
+        characterId = character.character_id;
+        return character;
+    } else {
+        return {exists: false};
+    }
 }
 
-function populateResultsDiv() {
+function updateSearchUI(character) {
     let results = document.getElementById("results");
     let session = document.getElementById("session");
     results.classList.add("visible");
-    session.classList.add("visible");
-    let children = results.children;
 
-    // character has more keys than results has children, is this ineffecient?
-    for (let stat in character) {
-        let child = children.namedItem(stat);
-        if (child) {
-            child.textContent += character[stat];
+    if (character.exists) {
+        session.classList.add("visible");
+
+        let children = results.children;
+        for (let stat in character) {
+            let child = children.namedItem(stat);
+            if (child) {
+                child.textContent += character[stat];
+            }
         }
+    } else {
+        results.textContent = "character not found";
     }
 }
 
+function searchForCharacter(name) {
+    getCharacter(name).then((character) => {
+        updateSearchUI(character);
+    });
+}
+
+const sessionButton = document.getElementById("session-button");
+let sessionActive = false;
+
+sessionButton.addEventListener("click", onSessionButtonClicked);
+
+function onSessionButtonClicked() {
+    if (sessionActive) {
+        endSession();
+    } else {
+        startSession();
+    }
+
+    sessionActive = !sessionActive;
+    updateSessionButtonUI();
+}
+
+function updateSessionButtonUI() {
+    sessionButton.classList.toggle("session-on", sessionActive);
+    sessionButton.textContent = sessionActive ? "End session" : "Start session";
+}
+
 function startSession() {
-    timeContainer = new Date(0);
-    timeElapsed = 0;
-    sessionKills = 0;
-    sessionDeaths = 0;
-
-    timeElement.textContent = "Time: 00:00:00";
-    kpmElement.textContent = "KPM: 0.0";
-
-    timeInterval = setInterval(() => {
-        timeElapsed += 1000;
-        timeContainer.setTime(timeElapsed);
-        displayTime();
-
-    }, 1000);
-
-    webSocket = new WebSocket("wss://push.planetside2.com/streaming?environment=ps2&service-id=s:supafarma");
+    session = {
+        kills: 0,
+        deaths: 0,
+        timeContainer: new Date(0),
+        timeElapsed: 0,
+        timeInterval: handleTimeUI()
+    };
+    
+    let webSocket = new WebSocket("wss://push.planetside2.com/streaming?environment=ps2&service-id=s:supafarma")
 
     webSocket.onopen = () => {
         let deathsCommand = {
             service: "event",
             action: "subscribe",
-            characters: [character.character_id],
+            characters: [characterId],
             eventNames: ["Death"]
         };
-
         webSocket.send(JSON.stringify(deathsCommand));
-
-        webSocket.onmessage = (message) => {
-            message = JSON.parse(message.data);
-            if (message.hasOwnProperty("payload")) {
-                handleKillData(message.payload);
-            }
-        };
     };
-}
 
-function displayTime(sessionTime) {
-    let timeString = timeContainer.toUTCString().split(" ")[4]
-    timeElement.textContent = `Time: ${timeString}`;
+    webSocket.onmessage = (message) => {
+        message = JSON.parse(message.data);
+        if (message.hasOwnProperty("payload")) {
+            onKillEvent(message.payload);
+        }
+    };
 
-    let minutesElapsed = timeContainer.getTime()/1000/60;
-    let kpm = (sessionKills/minutesElapsed).toFixed(1);;
-    kpmElement.textContent = `KPM: ${kpm}`;
+    session.webSocket = webSocket;
 }
 
 function endSession() {
-    webSocket.close();
-    clearInterval(timeInterval);
+    session.webSocket.close();
+    clearInterval(session.timeInterval);
 }
 
-function handleKillData(payload) {
-    let eventResult, enemyCharacterId, enemyLoadoutId;
-
-    if (payload.attacker_character_id == character.character_id) {
-        eventResult = "kill";
-        sessionKills += 1;
-
-        enemyCharacterId = payload.character_id;
-        enemyLoadoutId = payload.character_loadout_id;
-    } else {
-        eventResult = "death";
-        sessionDeaths += 1;
-
-        enemyCharacterId = payload.attacker_character_id;
-        enemyLoadoutId = payload.attacker_loadout_id;
-    }
-
-    let enemyCharacterUrl = `character/?character_id=${enemyCharacterId}&c:show=character_id,name.first,battle_rank.value,prestige_level&c:join=characters_stat_history^list:1^terms:stat_name=kills'stat_name=deaths^show:stat_name'all_time^inject_at:stats&c:tree=start:stats^field:stat_name`;
-    let weapUrl = `item/?item_id=${payload.attacker_weapon_id}&c:show=name.en`;
-
-    let killData = {
-        eventResult: eventResult,
-        loadout: loadoutList[enemyLoadoutId]
-    };
-
-    // TODO Convert these things to check is the object exists properly..
-    getJSON(enemyCharacterUrl, (data) => {
-        // returned can be 0 or > 0 
-        // perhaps we don't care what returned is, as we already handle the data not existing.
-        if (data.returned) {
-            let rawChar = (data.character_list ?? [])[0] ?? {};
-            let kills = ((rawChar.stats ?? {}).kills ?? {}).all_time ?? 0;
-            let deaths = ((rawChar.stats ?? {}).deaths ?? {}).all_time ?? 0;
-            killData.name = (rawChar.name ?? {}).first ?? "N/A";
-            killData.level = (rawChar.battle_rank ?? {}).value ?? "N/A";
-            killData.prestige = rawChar.prestige_level ?? "N/A";
-            killData.kd = (kills/deaths).toFixed(1);
-        }
-        getJSON(weapUrl, (data) => {
-            if (data.returned) {
-                let rawWeapon = (data.item_list ?? [])[0] ?? {};
-                killData.weapName = (rawWeapon.name ?? {}).en ?? "N/A";
-                displayKillData(killData);
-            } else {
-                killData.weapName = "API Problems";
-                displayKillData(killData);
-            }
-        });
+function onKillEvent(payload) {
+            console.log("test");
+    getKillData(payload).then((killData) => {
+        updateSessionUI(killData);
     });
 }
 
-function displayKillData(killData) {
+let timeElement = document.getElementById("session-time");
+let kpmElement = document.getElementById("session-kpm");
+
+function handleTimeUI() {
+    // is  this bad code?
+    return setInterval(() => {
+        session.timeElapsed += 1000;
+        session.timeContainer.setTime(session.timeElapsed);
+
+        let timeString = session.timeContainer.toUTCString().split(" ")[4];
+        let minutesElapsed = session.timeContainer.getTime() / 1000 / 60;
+
+        let kpm = (session.kills / minutesElapsed).toFixed(1);
+        
+        timeElement.textContent = `Time: ${timeString}`;
+        kpmElement.textContent = `KPM: ${kpm}`;
+
+    }, 1000);
+}
+
+async function getKillData(payload) {
+    let eventResult, enemyCharacterId, enemyLoadoutId;
+    if (payload.attacker_character_id == characterId) {
+        enemyCharacterId = payload.character_id;
+        enemyLoadoutId = payload.character_loadout_id;
+        eventResult = "kill"
+        session.kills += 1
+    } else {
+        enemyCharacterId = payload.attacker_character_id;
+        enemyLoadoutId = payload.attacker_loadout_id;
+        eventResult = "death"
+        session.deaths += 1
+    }
+
+    let enemyUrl = apiUrl + `character/?character_id=${enemyCharacterId}&c:show=character_id,name.first,battle_rank.value,prestige_level&c:join=characters_stat_history^list:1^terms:stat_name=kills'stat_name=deaths^show:stat_name'all_time^inject_at:stats&c:tree=start:stats^field:stat_name`;
+    let weaponUrl = apiUrl + `item/?item_id=${payload.attacker_weapon_id}&c:show=name.en`;
+
+    // Potentially use Promise.all() here?
+    let enemy = await fetch(enemyUrl);
+    let weapon = await fetch(weaponUrl);
+
+    let enemyJson = await enemy.json();
+    let weaponJson = await weapon.json();
+
+    let enemyData = (enemyJson.character_list ?? [])[0] ?? {};
+    let kills = ((enemyData.stats ?? {}).kills ?? {}).all_time ?? 0;
+    let deaths = ((enemyData.stats ?? {}).deaths ?? {}).all_time ?? 0;
+
+    let weaponData = (weaponJson.item_list ?? [])[0] ?? {};
+
+    // potentially use a list if we aren't going to format using key name
+    let killData = {
+        eventResult: eventResult,
+        name: (enemyData.name ?? {}).first ?? "N/A",
+        level: (enemyData.battle_rank ?? {}).value ?? "N/A",
+        prestige: enemyData.prestige_level ?? "N/A",
+        kd: (kills / deaths).toFixed(1),
+        loadout: loadoutList[enemyLoadoutId].substring(0,
+                 loadoutList[enemyLoadoutId].indexOf(" ")),
+        faction: loadoutList[enemyLoadoutId].substring(
+                 loadoutList[enemyLoadoutId].indexOf(" ") + 1),
+        weaponName: (weaponData.name ?? {}).en ?? "N/A"
+    }
+    
+    console.log(killData);
+
+    return killData
+}
+
+const kdElement = document.getElementById("session-kd");
+const sessionEvents = document.getElementById("session-events");
+
+function updateSessionUI(killData) {
     let div = document.createElement("div");
     div.className = `${killData.eventResult} event`;
-    let killDataString = 
     div.textContent = Object.values(killData).join(", ");
 
     sessionEvents.insertBefore(div, sessionEvents.childNodes[0]);
 
-    let kd = (sessionKills/sessionDeaths).toFixed(1);
-    kd = isFinite(kd) ? kd : sessionKills;
+    let kd = (session.kills
+              / session.deaths === 0 ? 1 : session.deaths).toFixed(1);
 
-    kdElement.textContent = `Kills: ${sessionKills},
-                             Deaths:${sessionDeaths}, KD: ${kd}`;
+    kdElement.textContent = `Kills: ${session.kills},
+                             Deaths: ${session.deaths}, KD: ${kd}`;
 }
